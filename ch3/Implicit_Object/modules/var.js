6c266baa06e6ecb7d4b365ef104c13afe58b4fcd
Jeongho Han <jungho1109@gmail.com>
2020-01-25 18:55:21 -0800

    module, exports different way

---
+++ ch3/Implicit_Object/modules/var.js
@@ -0,0 +1,12 @@
+// ===== option 1 =====
+// const odd ='Odd Number';
+// const even = 'Even Number';
+
+// module.exports={
+//   odd,
+//   even,
+// }
+
+
+exports.odd = 'Odd Number';
+exports.even = 'Even Number';
--- ch3/modules/var.js
+++
@@ -1,7 +0,0 @@
-const odd ='Odd Number';
-const even = 'Even Number';
-
-module.exports={
-  odd,
-  even,
-}
